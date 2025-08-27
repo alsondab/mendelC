1ead49658c328e8b08aa2a5ea497fd49eef4f80d
Alson Dabo <alsondab9@gmail.com>
2025-08-27 23:50:29 +0000

    7commit

---
+++ .history/next.config_20250827234536.ts
@@ -0,0 +1,20 @@
+import type { NextConfig } from 'next'
+import withNextIntl from 'next-intl/plugin'
+
+const nextConfig: NextConfig = withNextIntl()({
+  /* config options here */
+  experimental: {
+    esmExternals: 'loose',
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'utfs.io',
+        port: '',
+      },
+    ],
+  },
+})
+
+export default nextConfig
